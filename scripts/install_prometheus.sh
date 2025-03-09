1b2e9804404f30dc5b5330eb439bcb1c35e5437e
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-03-09 16:27:05 -0500

    [nc] installation workflows
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/install_prometheus.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -eo pipefail
+source bash-functions.sh
+
+cluster_name=$1
+prometheus_chart_version=$(jq -er .prometheus_chart_version environments/$cluster_name.json)
+echo "prometheus version $prometheus_chart_version"
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# perform trivy scan of chart with install configuration
+trivyScan "prometheus-community/prometheus" "prometheus" "$CHART_VERSION" "prometheus-values/$cluster_name-values.yaml"
+
+helm upgrade --install prometheus prometheus-community/prometheus \
+             --version $prometheus_chart_version \
+             --namespace observe \
+             --values prometheus-values/$cluster_name-values.yaml
+
+sleep 15
+
+# deploy oauth2 authentication proxy for alertmanager
+helm upgrade --install prometheus-alertmanager-oauth2-proxy chart/oauth2-proxy \
+     --namespace observe \
+     --set env.OAUTH2_PROXY_CLIENT_ID="${OAUTH2_PROXY_CLIENT_ID}" \
+     --set env.OAUTH2_PROXY_CLIENT_SECRET="${OAUTH2_PROXY_CLIENT_SECRET}" \
+     --set env.OAUTH2_PROXY_COOKIE_SECRET="${OAUTH2_PROXY_COOKIE_SECRET}" \
+     --values chart/oauth2-proxy/values.yaml \
+     --values prometheus-values/$cluster_name-alertmanager-oauth2-proxy-values.yaml \
+
+# deploy oauth2 authentication proxy for prometheus server
+helm upgrade --install prometheus-server-oauth2-proxy chart/oauth2-proxy \
+     --namespace observe \
+     --set env.OAUTH2_PROXY_CLIENT_ID="${OAUTH2_PROXY_CLIENT_ID}" \
+     --set env.OAUTH2_PROXY_CLIENT_SECRET="${OAUTH2_PROXY_CLIENT_SECRET}" \
+     --set env.OAUTH2_PROXY_COOKIE_SECRET="${OAUTH2_PROXY_COOKIE_SECRET}" \
+     --values chart/oauth2-proxy/values.yaml \
+     --values prometheus-values/sbx-i01-aws-us-east-1-prometheus-oauth2-proxy-values.yaml \
---
+++ scripts/observability_demo_setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+cluster_name=$1
+
+# create observability demo namespace
+echo "observability demo namespace"
+kubectl apply -f tpl/observability-demo-namespace.yaml
+
+# create prometheus ebs-csi persistent volume claim
+echo "create persistent volume claims for prometheus"
+cat <<EOF > prometheus-values/pvc.yaml
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: prometheus-ebs-pvc
+  namespace: observe
+spec:
+  accessModes:
+    - ReadWriteOnce
+  storageClassName: ${cluster_name}-ebs-csi-dynamic-storage
+  resources:
+    requests:
+      storage: 8Gi
+EOF
+kubectl apply -f prometheus-values/pvc.yaml
